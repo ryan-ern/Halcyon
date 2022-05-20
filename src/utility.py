546362804272c5d3e42f9a0d02450f4ad6b9c1a9
martatiaamanda <martatiamandaa03@gmail.com>
2022-05-20 20:14:29 +0700

    load gambar bullet dan membuat rumus background

--- src/utility.py
+++ src/utility.py
@@ -26,3 +26,19 @@ WINDOW = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Halcyon")
 ICON = pygame.transform.scale(PLAYER, (50, 50))
 pygame.display.set_icon(ICON)
+
+#image bullet
+PLAYER_BULLET = pygame.image.load(os.path.join("assets", "pbullet.png"))
+ENEMY_BULLET1 = pygame.image.load(os.path.join("assets", "ebullet1.png"))
+ENEMY_BULLET2 = pygame.image.load(os.path.join("assets", "ebullet2.png"))
+ENEMY_BULLET3 = pygame.image.load(os.path.join("assets", "ebullet3.png"))
+
+#how to play
+HOWTOPLAY = pygame.image.load(os.path.join("assets", "howtoplay.png"))
+
+#background
+BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("assets", "BG.png")), (WIDTH, HEIGHT))
+BG_WIDTH = BACKGROUND.get_width()
+BG_RECT = BACKGROUND.get_rect()
+SCROLL = 0
+TILES = math.ceil(WIDTH  / BG_WIDTH) + 1
